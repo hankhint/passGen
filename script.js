6d03823d32aa09044e5a3f91b38c6ef22ec19d35
HankHint <hankhint@gmail.com>
2021-10-21 13:57:43 -0400

    added some pseudocode for logic in js file

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+
+//get user input for password parameters
+// which characters?  
+// how many characters? var = chacterAmount
+
+//create an array of all potential password characters var = passwordChar
+// use if statements and concat it together
+
+
+// use a for loop with max value of characterAmount
+// generate a random number between 0 and passwordChar.length
+// use the random number to pull one random character from passwordChar array
+// append the random character from passwordChar to var = password
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password output
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
